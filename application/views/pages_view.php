32815c711e51f212425e45eeb4126abc9227e981
IbadSyed <43743123+IbadSyed@users.noreply.github.com>
2018-10-01 15:18:00 +0100

    Add files via upload

---
+++ application/views/pages_view.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <!-- <script src="js/slideshow.js"></script>-->
+        <!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="css/slideshow.css">
+
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="container">
+	        <div class="jumbotron">
+	  			<h1>Hello developer!</h1>
+	  			<p>This data represents the structure of an Elucidat e-learning course. Your task, if you choose to accept it, is to show this in an interesting, and useful way.</p>
+	  		
+	  			<ul>
+		  			<li>We've included some useful libraries to get you going. Feel free to use these, or use your own. Up to you.</li>
+		  			<li>The data represents a set of pages. The Learner will progress through the pages in order, following the links within the pages.</li>
+		  			<li>There are often multiple links within a page, so the Learner may not progress in a linear order - these are shown in the 'other links' variables - and (hint) are the most interesting bit of this test.</li>
+		  			<li>This display is intended for the author of the course, who is wanting to understand how a Learner might progress through the course.</li>
+		  			<li>You must display the structure in an interesting way.</li>
+		  			<li>Feel free to add to the data if you want - if you think an extra bit of page data would be useful to a content author - add it. Extra data could be related to the editing process, or to how Learners are interacting with the page.</li>
+		  			<li>What it looks like is important.</li>
+		  			<li>You must make it interactive (for example implement a filter).</li>
+		  			<li>Please make sure that your filter is implemented server-side (to force you to demonstrate your PHP).</li>
+		  			<li>Documentation is important. So are Unit tests.</li>
+	  			</ul>
+	  		</div>
+
+	  		<h2>Course data:</h2>
+  			<!-- <pre><code> -->
+  			<?php 
+  			$fields = array('name', 'page_code', 'children', 'other_links', 'only_show_page_if', 'page_awards_badge', 'grab'); 
+  			//$output .= '<table>';
+  			$output .= "<div style='float: left;'><div class='slideshow-container'>";
+  			$totalslides = count($pages);
+				for($i=0; $i<count($pages); $i++){
+					//$output .= '<tr>';
+					$currentslide = ($i+1);
+					$output .= "<div class='mySlides'>";
+					$output .=  "<div class='numbertext'>$currentslide / $totalslides</div>";
+					$text = '';
+					foreach($fields as $fieldname){
+						
+						switch ($fieldname){
+							case "name":
+								$text .= $pages[$i][$fieldname]. " &nbsp;";
+								$gotoSlide .= "<a onclick='gotoSlide($currentslide)'>Go to slide ".$pages[$i][$fieldname]."</a><br>";
+								break;
+							case "grab":
+									$output .= "<img src = '".$pages[$i][$fieldname]."' style='width:700px;'>";
+								break;
+							case "page_code":
+								//$text .= "Code: " . $pages[$i][$fieldname];
+								break;
+							case "children":
+								break;
+							case "other_links":
+								break;
+							case "only_show_page_if":
+								break;
+								case "page_awards_badge":
+								break;
+							case "only_show_page_if":
+								break;
+							
+							
+						}
+						$output .= "<div class='text'>".$text."</div>";
+					}
+					$output .= "</div>";
+
+				}
+			//$output .= '</table>'; 
+			
+			$output .= '<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+						<a class="next" onclick="plusSlides(1)">&#10095;</a></div>'; 
+			echo $output;			
+  			
+  			//echo json_encode( $pages, JSON_PRETTY_PRINT ); 
+  			?>
+  			<!-- </code></pre> -->
+  			
+        </div>
+        <?php echo "<div style='float:right;'>".$gotoSlide."</div>";?>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+		<!-- Latest compiled and minified Bootstrap -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<!-- CanJS - an alternative to Angular - allows for interesting client side interactions-->
+		<script src="http://canjs.com/release/2.2.4/can.jquery.js"></script>
+		<!-- D3 is a brilliant library for data visualisations -->
+		<script src="http://d3js.org/d3.v3.min.js"></script>
+		<!-- Now over to you - have fun! -->
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/slideshow.js"></script>
+
+        
+
+    </body>
+</html>
